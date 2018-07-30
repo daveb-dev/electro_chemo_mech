8bcc52a8eddb20db6805a939bc04d2e11f0db5a7
Srinath Chakravarthy <s.chakravarthy@neu.edu>
2018-07-30 10:09:05 -0400

    Add Linear Stress strain for finite strain materials

---
+++ include/materials/ComputeTlLinearStress.h
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   ComputeTLNeoHookeanPkStress.h
+ * Author: srinath
+ *
+ * Created on July 24, 2018, 10:07 AM
+ */
+
+#ifndef COMPUTETLNEOHOOKEANPKSTRESS_H
+#define COMPUTETLNEOHOOKEANPKSTRESS_H
+
+#include "ComputeTlFiniteStrainStress.h"
+
+//Forward Declaration
+class ComputeTLNeoHookeanPkStress;
+
+template <>
+InputParameters validParams<ComputeTLNeoHookeanPkStress>();
+
+class ComputeTLNeoHookeanPkStress : public ComputeTlFiniteStrainStress 
+
+{
+public:
+    ComputeTLNeoHookeanPkStress(const InputParameters & parameters);
+protected:
+    virtual void computeQpStress();
+private:
+
+};
+
+#endif /* COMPUTETLNEOHOOKEANPKSTRESS_H */
+
