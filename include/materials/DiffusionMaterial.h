8db0d74fa40447a876d60b3049c391b89c03ef92
Srinath Chakravarthy <s.chakravarthy@neu.edu>
2018-08-21 15:31:20 -0400

    Made diffusion material generic diffusion material

--- include/materials/DiffusionMaterial.h
+++ include/materials/DiffusionMaterial.h
@@ -15,13 +15,11 @@
 #define DIFFUSIONMATERIAL_H
 
 #include "Material.h"
-#include "RankTwoTensor.h"
-#include "RankFourTensor.h"
-#include "RotationTensor.h"
-#include "DerivativeMaterialInterface.h"
+
 
 //Forward Declrartion
 class DiffusionMaterial;
+class Function;
 
 template<>
 InputParameters validParams<DiffusionMaterial>();
@@ -32,14 +30,29 @@ class DiffusionMaterial : public Material
 public:
     DiffusionMaterial(const InputParameters & parameters);
 protected:
-    virtual void computeProerties();
-    virtual void initialSetup() override;
+    virtual void computeProperties();
 
+    const bool _has_conc;
+    const VariableValue & _concentration;
+    
+    // Local variables
     const Real _M;
+    const Real _gamma;
     const Real _beta;
     const Real _rhoh;
     const Real _cmax;
+    const Real _R; 
+    const Real _temp;
+    
+    // Material Property variables
     MaterialProperty<Real> & _mobility;
+    MaterialProperty<Real> & _mobility_dC;
+    Function * _mobility_concentration_function;
+    
+    MaterialProperty<Real> & _activity_coefficient;
+    MaterialProperty<Real> & _activity_coefficient_dC;
+    Function * _actitivity_coefficient_concentration_function;
+      
     MaterialProperty<Real> & _lattice_misfit;
     MaterialProperty<Real> & _molar_volume;
     MaterialProperty<Real> & _max_concentration;
